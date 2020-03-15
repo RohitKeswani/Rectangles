2ca3b7754d1ef12cb731e00592b648926a46143d
RohitKeswani <rhareshk@gmail.com>
2020-03-15 01:33:44 -0400

    Added containment logic

--- src/main/java/Controllers/Analyzer.java
+++ src/main/java/Controllers/Analyzer.java
@@ -5,9 +5,12 @@ import models.Rectangle;
 public class Analyzer {
     public boolean[] analyze(Rectangle firstRectangle, Rectangle secondRectangle) {
         boolean[] status = new boolean[3];
-        boolean contained = checkContainment(firstRectangle, secondRectangle);
-        boolean adjacent = checkAdjacent(firstRectangle, secondRectangle);
-        boolean intersect = checkIntersection(firstRectangle, secondRectangle);
+        boolean contained, adjacent, intersect = false;
+        contained = checkContainment(firstRectangle, secondRectangle);
+        adjacent = checkAdjacent(firstRectangle, secondRectangle);
+        if(!contained) {
+            intersect = checkIntersection(firstRectangle, secondRectangle);
+        }
         status[0] = contained;
         status[1] = adjacent;
         status[2] = intersect;
@@ -15,8 +18,21 @@ public class Analyzer {
     }
 
     private boolean checkIntersection(Rectangle firstRectangle, Rectangle secondRectangle) {
-        boolean contained = false;
-        return contained;
+        boolean intersect = false;
+        if(firstRectangle.getBottomRight().getX() < secondRectangle.getBottomRight().getX())
+            System.out.println("java is mad");
+        if((firstRectangle.getBottomRight().getX() > secondRectangle.getTopLeft().getX() &&
+                firstRectangle.getBottomRight().getX() < secondRectangle.getBottomRight().getX()) ||
+                //TODO following condition returns true for R1=25,60 and R2=40,20
+                (firstRectangle.getBottomRight().getX() > secondRectangle.getTopLeft().getX() &&
+                firstRectangle.getTopLeft().getX() < secondRectangle.getTopLeft().getX()))
+            intersect = true;
+        if((firstRectangle.getTopLeft().getY() > secondRectangle.getBottomRight().getY() &&
+                firstRectangle.getBottomRight().getY() < secondRectangle.getBottomRight().getY())||
+                (firstRectangle.getTopLeft().getY() < secondRectangle.getTopLeft().getY() &&
+                        secondRectangle.getBottomRight().getY() < firstRectangle.getTopLeft().getY()))
+            intersect = true;
+        return intersect;
     }
 
     private boolean checkAdjacent(Rectangle firstRectangle, Rectangle secondRectangle) {
@@ -25,7 +41,17 @@ public class Analyzer {
     }
 
     private boolean checkContainment(Rectangle firstRectangle, Rectangle secondRectangle) {
-        boolean intersect = false;
-        return intersect;
+        boolean contained = false;
+        if(firstRectangle.getTopLeft().getX() < secondRectangle.getTopLeft().getX() &&
+                firstRectangle.getTopLeft().getY() > secondRectangle.getTopLeft().getY() &&
+                firstRectangle.getBottomRight().getX() > secondRectangle.getBottomRight().getX() &&
+                firstRectangle.getBottomRight().getY() < secondRectangle.getBottomRight().getY())
+            contained = true;
+        if(secondRectangle.getTopLeft().getX() < firstRectangle.getTopLeft().getX() &&
+        secondRectangle.getTopLeft().getY() > firstRectangle.getTopLeft().getY() &&
+        secondRectangle.getBottomRight().getX() > firstRectangle.getBottomRight().getX() &&
+        secondRectangle.getBottomRight().getY() < firstRectangle.getBottomRight().getY())
+            contained = true;
+        return contained;
     }
 }
--- src/main/java/Execute.java
+++ src/main/java/Execute.java
@@ -4,21 +4,21 @@ import models.Rectangle;
 
 public class Execute {
     public static void main(String[] args) {
-        Coordinate topLeft = new Coordinate(30,50);
-        Coordinate bottomRight = new Coordinate(50, 30);
+        Coordinate topLeft = new Coordinate(30 ,70);
+        Coordinate bottomRight = new Coordinate(60, 50);
         Rectangle firstRectangle = new Rectangle(topLeft, bottomRight);
-        topLeft = new Coordinate(60, 40);
-        bottomRight = new Coordinate(80,20);
-        Rectangle secondRectangle = new Rectangle(topLeft, bottomRight);
+        Coordinate left = new Coordinate(40, 60);
+        Coordinate right = new Coordinate(30,30);
+        Rectangle secondRectangle = new Rectangle(left, right);
         Analyzer analyzer = new Analyzer();
         boolean[] status = analyzer.analyze(firstRectangle, secondRectangle);
         if(status[0])
-            System.out.println("Contained");
+            System.out.println("Containment");
         if(status[1])
             System.out.println("Adjacent");
         if(status[2])
-            System.out.println("Intersect");
-        else
+            System.out.println("Intersection - No Containment");
+        if(!status[0] && !status[1] && !status[2])
             System.out.println("Nothing");
     }
 }
